dfc6560290b0dac9d97819ab0c39809936d8d9dc
Bruno Braga <dev@viladosite.com.br>
2016-04-18 11:47:34 -0300

    envio dos arquivos.

---
+++ contratos/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Sistema VDS</title>
+<style type="text/css">
+body,td,th {
+	color: #3A3A3A;
+}
+</style>
+
+
+
+
+</head>
+
+<body>
+<p>Página inicial</p>
+<p>&nbsp;</p>
+<p>Link 1</p>
+<p>Link 2</p>
+<p>Link 3</p>
+<p>Link 4</p>
+<p><a href="at_gerar.php">Gerar Contrato</a></p>
+</body>
+</html>
