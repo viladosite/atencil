c782ed31f784a5e8172d0db2a473db5745930dd1
bruno <dev@viladosite.com.br>
2016-05-30 14:24:21 -0300

    drawer externo ao painel

---
+++ drawer.php
@@ -0,0 +1,27 @@
+<div class="mdl-layout__drawer"> <!-- criação do drawer padrão -->
+    	<span class="mdl-layout-title">Menu Principal</span>
+    	<nav class="mdl-navigation">
+      		<a class="mdl-navigation__link" href="">
+            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">perm_phone_msg</i>
+            Atendimentos</a>
+
+      		<a class="mdl-navigation__link" href="">
+            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">person_pin</i>
+            Clientes</a>
+
+      		<a class="mdl-navigation__link" href="">
+            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">group</i>
+            Usuários</a>
+
+			<a class="mdl-navigation__link" href="">
+            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">close</i>
+            Sair</a>
+            
+
+            
+       		<a class="mdl-navigation__link" href="">
+            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">help</i>
+            Ajuda</a>
+            
+    	</nav>
+  	</div>
--- painel.php
+++ painel.php
@@ -29,7 +29,7 @@ if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] < $nivel_necess
 		}
 
 		#busca{
-			width: 40%!important;
+			width: 50%!important;
 		}
 	</style>
 	 
@@ -70,33 +70,9 @@ if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] < $nivel_necess
 		
 
 				
-  	<div class="mdl-layout__drawer"> <!-- criação do drawer padrão -->
-    	<span class="mdl-layout-title">Menu Principal</span>
-    	<nav class="mdl-navigation">
-      		<a class="mdl-navigation__link" href="">
-            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">perm_phone_msg</i>
-            Atendimentos</a>
-
-      		<a class="mdl-navigation__link" href="">
-            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">person_pin</i>
-            Clientes</a>
-
-      		<a class="mdl-navigation__link" href="">
-            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">group</i>
-            Usuários</a>
-
-			<a class="mdl-navigation__link" href="">
-            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">close</i>
-            Sair</a>
-            
-
-            
-       		<a class="mdl-navigation__link" href="">
-            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">help</i>
-            Ajuda</a>
-            
-    	</nav>
-  	</div>
+  	<?php 
+  	include "drawer.php";
+  	?>
 		
 	<!-- grids -->
 	<div class="mdl-grid">
