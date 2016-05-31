efd4571abef4d17945d3acf5e5de25b8080b904f
Diogo Fagundes [Vila do Site] <admin@viladosite.com.br>
2016-05-31 13:52:42 -0300

    Finalização do sistema de logout
    Sistema finalizado precisando apenas de ajustes visuais no futuro.

--- includes/at_logout.php
+++ includes/at_logout.php
@@ -1,8 +1,8 @@
-	<?php
-		session_start();
+<?php
+session_start();
 //		if(isset($_SESSION)){ // Verifica a sessão e caso não esteja iniciada a inicia
-        session_destroy(); // Destrói a sessão limpando todos os valores salvos
-        header("Location: index.html"); // Redireciona o visitante
-		exit();
+session_destroy(); // Destrói a sessão limpando todos os valores salvos
+header("Location: ../index.html"); // Redireciona o visitante
+exit();
 //		}
-	?>
+?>
