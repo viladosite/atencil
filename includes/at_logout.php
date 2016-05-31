26b15d788f891c9f0ed7f92f161549d35b25d7a9
Diogo Fagundes [Vila do Site] <admin@viladosite.com.br>
2016-05-31 13:47:05 -0300

    Ajustes no sistema de logout

--- includes/at_logout.php
+++ includes/at_logout.php
@@ -1,5 +1,8 @@
 	<?php
-        session_start(); // Inicia a sessão
+		session_start();
+//		if(isset($_SESSION)){ // Verifica a sessão e caso não esteja iniciada a inicia
         session_destroy(); // Destrói a sessão limpando todos os valores salvos
-        header("Location: index.php"); exit; // Redireciona o visitante
+        header("Location: index.html"); // Redireciona o visitante
+		exit();
+//		}
 	?>
