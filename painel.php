f6798d562c78b7b93b17539beb4f323aa9b48ecc
bruno <dev@viladosite.com.br>
2016-06-01 16:54:20 -0300

    Inclusão do footer
    incluido provisoriamente no painel para teste

--- painel.php
+++ painel.php
@@ -85,6 +85,19 @@ if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] < $nivel_necess
     
 		</main>
 		
+		<!-- Inclusão do mini footer provisoriamente , lgo será passado para um arquivo externo -->
+
+		<footer class="mdl-mini-footer">
+  			<div class="mdl-mini-footer--left-section">
+  			<div class="mdl-logo">Atencil - 2016</div>
+  			<ul class="mdl-mini-footer--link-list">
+  			<li><a href="#">Vila do Site</a></li>
+  			<li><a href="#">Pagina do Facebook</a></li>
+  			</ul>
+
+  			</div>
+	  	</footer>
+
 	</div>
 
 </body>
