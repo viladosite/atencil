8a0c70adc3240a0b917b77aa0c093f326739fd35
Diogo Fagundes [Vila do Site] <admin@viladosite.com.br>
2016-06-01 17:04:53 -0300

    Pequenos ajustes de código

--- painel.php
+++ painel.php
@@ -86,12 +86,11 @@ if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] < $nivel_necess
 
 		<footer class="mdl-mini-footer">
   			<div class="mdl-mini-footer--left-section">
-  			<div class="mdl-logo">Atencil - 2016</div>
-  			<ul class="mdl-mini-footer--link-list">
-  			<li><a href="#">Vila do Site</a></li>
-  			<li><a href="#">Pagina do Facebook</a></li>
-  			</ul>
-
+  				<div class="mdl-logo">Atencil - 2016</div>
+  				<ul class="mdl-mini-footer--link-list">
+	  			<li><a href="#">Vila do Site</a></li>
+  				<li><a href="#">Pagina do Facebook</a></li>
+  				</ul>
   			</div>
 	  	</footer>
 
