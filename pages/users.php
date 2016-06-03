8d932c99faef8aca90e9a3e7288993ef6dc681f6
Diogo Fagundes [Vila do Site] <admin@viladosite.com.br>
2016-06-03 12:48:58 -0300

    Alteração de páginas

--- pages/users.php
+++ pages/users.php
@@ -1,31 +1,19 @@
-
 <?php
 // A sessão precisa ser iniciada em cada página diferente
 if (!isset($_SESSION)) session_start();
-
 // Pega as configurações e permissões do arquivo de config
 require "config.php";
-
-// Início do conteúdo da página
-$conteudo = '
-
-<b> Conteúdo da página de usuários </b>
-
-';
-// Fim do conteúdo da página
-////////////////////////////////////////
-// Início do conteúdo do erro
-$erro = '
-
-<b> Você não tem permissão para visualizar este conteúdo </b>
-
-';
-// Fim do conteúdo do erro
 ?>
 
 
 
 <div id="content">
 <!-- Verifica se a permissão do usuário é maior ou igual à necessária e monta as exibições -->
-<?php if ($_SESSION['UsuarioNivel'] >= $perm_edit_users): $conteudo; else:$erro; endif; ?>
+<?php if ($_SESSION['UsuarioNivel'] >= $perm_edit_users): ?>
+<b> Conteúdo da página de usuários </b>
+
+<?php else: ?>
+<b> Você não tem permissão para visualizar este conteúdo </b>
+
+<?php endif; ?>
 </div>
