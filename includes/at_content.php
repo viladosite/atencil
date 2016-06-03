57233c35afcf483d4ec84d5e92c26ca0710e01c8
bruno <dev@viladosite.com.br>
2016-06-03 14:22:38 -0300

    estilização dos cards
    alteração no content e no css para padronizar os cards

--- includes/at_content.php
+++ includes/at_content.php
@@ -3,28 +3,32 @@
 	<!-- Criação dos grids -->
 	<div class="mdl-grid">
   		<div class="mdl-cell mdl-cell--4-col">
-		  				
-		  				<div class="at-recente-card mdl-card mdl-shadow--4dp demonstracao-card">
+		  			<!--	<h3>Atendimentos Recentes</h3> -->
+		  				<div class="mdl-card mdl-shadow--4dp demonstracao-card">
 		  					<div class="mdl-card__title">
 		  						<h2 class="mdl-card__title-text">Atendimentos Recentes</h2>
 		  					</div>
 		  					<div class="mdl-card__supporting-text">
 		  						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		  						tempor incididunt ut labore et dolore magna aliqua.
+		  						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		  						tempor incididunt ut labore et dolore magna aliqua.
 		  					</div>
 
 		  					<div class="mdl-card__actions mdl-card--border">
-		  						<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary">botão</button>
+		  						<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent mdl-button--raised">botão</button>
 		  					</div>
 
+		  					<div class="mdl-card__menu">
 		  					<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
 		  						<i class="material-icons">share</i>	
 		  					</button>
+		  					</div>
   						</div>
   		</div>
 
   		<div class="mdl-cell mdl-cell--4-col">
-		  				
+		  				<!--	<h3>Atendimentos Recentes</h3> -->
 		  				<div class="mdl-card mdl-shadow--3dp demonstracao-card">
 		  					<div class="mdl-card__title">
 		  						<h2 class="mdl-card__title-text">Atendimentos Pendentes</h2>
@@ -32,20 +36,24 @@
 		  					<div class="mdl-card__supporting-text">
 		  						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		  						tempor incididunt ut labore et dolore magna aliqua.
+		  						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		  						tempor incididunt ut labore et dolore magna aliqua.
 		  					</div>
 
 		  					<div class="mdl-card__actions mdl-card--border">
-		  						<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary">botão</button>
+		  						<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent mdl-button--raised">botão</button>
 		  					</div>
 
+		  					<div class="mdl-card__menu">
 		  					<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
 		  						<i class="material-icons">share</i>	
 		  					</button>
+		  					</div>
   						</div>
   		</div>
 
   		<div class="mdl-cell mdl-cell--4-col">
-		  				
+		  				<!--	<h3>Atendimentos Recentes</h3> -->
 		  				<div class="mdl-card mdl-shadow--3dp demonstracao-card">
 		  					<div class="mdl-card__title">
 		  						<h2 class="mdl-card__title-text">Usuários</h2>
@@ -53,20 +61,24 @@
 		  					<div class="mdl-card__supporting-text">
 		  						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		  						tempor incididunt ut labore et dolore magna aliqua.
+		  						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		  						tempor incididunt ut labore et dolore magna aliqua.
 		  					</div>
 
 		  					<div class="mdl-card__actions mdl-card--border">
-		  						<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary">botão</button>
+		  						<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent mdl-button--raised">botão</button>
 		  					</div>
 
+		  					<div class="mdl-card__menu">
 		  					<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
 		  						<i class="material-icons">share</i>	
 		  					</button>
+		  					</div>
   						</div>
   		</div>
 
   		<div class="mdl-cell mdl-cell--4-col">
-		  				
+		  				<!--	<h3>Atendimentos Recentes</h3> -->
 		  				<div class="mdl-card mdl-shadow--3dp demonstracao-card">
 		  					<div class="mdl-card__title">
 		  						<h2 class="mdl-card__title-text">Clientes</h2>
@@ -74,15 +86,19 @@
 		  					<div class="mdl-card__supporting-text">
 		  						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		  						tempor incididunt ut labore et dolore magna aliqua.
+		  						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		  						tempor incididunt ut labore et dolore magna aliqua.
 		  					</div>
 
 		  					<div class="mdl-card__actions mdl-card--border">
-		  						<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary">botão</button>
+		  						<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent mdl-button--raised">botão</button>
 		  					</div>
 
+		  					<div class="mdl-card__menu">
 		  					<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
 		  						<i class="material-icons">share</i>	
 		  					</button>
+		  					</div>
   						</div>
   		</div>
 
