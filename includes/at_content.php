5ae738ef40f81584cfb9660e5b9c82162bf91842
bruno <dev@viladosite.com.br>
2016-06-08 14:00:33 -0300

    alteração de nome classe e css

--- includes/at_content.php
+++ includes/at_content.php
@@ -4,7 +4,7 @@
 	<div class="mdl-grid">
   		<div class="mdl-cell mdl-cell--4-col">
 		  			<!--	<h3>Atendimentos Recentes</h3> -->
-		  				<div class="mdl-card mdl-shadow--4dp demonstracao-card">
+		  				<div class="mdl-card mdl-shadow--4dp at-card">
 		  					<div class="mdl-card__title">
 		  						<h2 class="mdl-card__title-text">Atendimentos Recentes</h2>
 		  					</div>
@@ -33,7 +33,7 @@
 
   		<div class="mdl-cell mdl-cell--4-col">
 		  				<!--	<h3>Atendimentos Recentes</h3> -->
-		  				<div class="mdl-card mdl-shadow--3dp demonstracao-card">
+		  				<div class="mdl-card mdl-shadow--3dp at-card">
 		  					<div class="mdl-card__title">
 		  						<h2 class="mdl-card__title-text">Atendimentos Pendentes</h2>
 		  					</div>
@@ -61,7 +61,7 @@
 
   		<div class="mdl-cell mdl-cell--4-col">
 		  				<!--	<h3>Atendimentos Recentes</h3> -->
-		  				<div class="mdl-card mdl-shadow--3dp demonstracao-card">
+		  				<div class="mdl-card mdl-shadow--3dp at-card">
 		  					<div class="mdl-card__title">
 		  						<h2 class="mdl-card__title-text">Usuários</h2>
 		  					</div>
@@ -89,7 +89,7 @@
 
   		<div class="mdl-cell mdl-cell--4-col">
 		  				<!--	<h3>Atendimentos Recentes</h3> -->
-		  				<div class="mdl-card mdl-shadow--3dp demonstracao-card">
+		  				<div class="mdl-card mdl-shadow--3dp at-card">
 		  					<div class="mdl-card__title">
 		  						<h2 class="mdl-card__title-text">Clientes</h2>
 		  					</div>
