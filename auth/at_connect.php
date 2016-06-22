36ee5a7d0ba9aaaf5a402232a53f8c2656b07519
Diogo Fagundes [Vila do Site] <admin@viladosite.com.br>
2016-06-22 14:03:15 -0300

    Ajuste de conexão

--- auth/at_connect.php
+++ auth/at_connect.php
@@ -1,7 +1,7 @@
 <?php
 
 // Tenta se conectar ao servidor MySQL
-$mysql = mysql_connect($dbserver, $dbuser, $dbpass, $dbname) or trigger_error(mysql_error());
+$mysqli = mysql_connect($dbserver, $dbuser, $dbpass, $dbname) or trigger_error(mysql_error());
 
 // Tenta se conectar ao banco de dados MySQL prédefinido nas configurações
 mysql_select_db($dbname) or trigger_error(mysql_error());
