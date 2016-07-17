af26f3fb76cb243cc875002d813e9028b48aed4a
Diogo Fagundes [Vila do Site] <admin@viladosite.com.br>
2016-07-17 17:54:46 -0300

    Alterações do módulo de android
    Foram efetuadas alterações e amplos ajustes para que fosse possível iniciar o desenvolvimento do módulo de funcionamento para o aplicativo do android.

---
+++ auth/at_android.php
@@ -0,0 +1,41 @@
+<?php
+
+// Verifica se houve POST e se o usuário ou a senha é(são) vazio(s). Se estiver vazio retorna EMPTY
+    if (!empty($_POST) AND (empty($_POST['fielduser']) OR empty($_POST['fieldpass']))) {
+        echo "empty"; exit;
+    }
+      
+// Pega as definições de banco de dados do arquivo de config
+require '../config.php';
+
+// Faz a conexão com o banco
+require 'at_connect.php';
+
+//Monta as variáveis para os campos do form do app
+$user = $_GET['fielduser'];
+$pass = $_GET['fieldpass'];
+// $email = $_POST['email'];
+// $name = $_POST['fname'];
+// $name = $_POST['lname'];
+
+// Query que da um select no banco e se encontrar algo diferente de 1 como resultado, retorna erro
+	$sql = "SELECT `userid`, `userfname`, `userlname`, `usermail`, `userpermlvl`, `usercomp`, `usercomplvl`, `userstatus`, `userregdate` FROM `at_users` WHERE (`userlogin` = '".$user ."') AND (`userpass` = '". sha1($pass) ."') AND (`userstatus` = 1) LIMIT 1";
+
+    $query = mysqli_query($mysql, $sql);
+	
+    if (mysqli_num_rows($query) != 1) {
+
+// Mensagem de erro quando os dados são inválidos e/ou o usuário não foi encontrado retornando INVALID
+	echo "invalid";
+
+
+// Caso a resposta do select seja 1, o sistema encontrou o usuário, envia a resposta necessária para o que o app interprete, no caso o termo OK
+
+    } else {
+
+		echo "ok";
+     
+    }
+
+
+?>
