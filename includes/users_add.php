4a2dc3b38ac15eabc418fbfc8190df68e371ba37
bruno <bruckmrj@htmailcom>
2016-08-30 14:10:40 -0300

    pagina de confirmação usuario (testes)2

--- includes/users_add.php
+++ includes/users_add.php
@@ -22,9 +22,12 @@ $userstatus = $_POST ["userstatus"];
  $query = "INSERT INTO at_users ( userlogin , userpass , userfname , userlname , usermail ,userpermlvl , usercomp , userstatus )
     VALUES ('".$userlogin."', '".$userpass."', '".$userfname."', '".$userlname."', '".$usermail."', '".$userpermlvl."', '".$usercomp."', '".$userstatus."' )"; 
     if ($mysql->query($query) === TRUE)
-    /*{ $response = array("success" => true );
-    echo json_encode($response); } */
-     { $response = location.reload(); }
+        { window.alert("Usuário Adicionado com Sucesso!");
+            window.history.back();
+            }
+   /* { $response = array("success" => true );
+    echo json_encode($response); } */ 
+     /*{ $("#content").load("at_user_success.php"); }*/
      else {echo "Erro: " . $query . "<br>" . $mysql->error;}
      $mysql->close();
  ?>
