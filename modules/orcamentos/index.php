82117fb3f72bfc10a7c4827e7b5090cfbcd19d5f
Vila do Site <admin@viladosite.com.br>
2019-03-25 17:09:52 -0300

    Edição do conteúdo do orçamento

--- modules/orcamentos/index.php
+++ modules/orcamentos/index.php
@@ -1,14 +1,14 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Invoice</title>
+		<title>Orçamento - Vila do Site</title>
 		<link rel="stylesheet" href="style.css">
 		<link rel="license" href="https://www.opensource.org/licenses/mit-license/">
 		<script src="script.js"></script>
 	</head>
 	<body>
 		<header>
-			<h1>Invoice</h1>
+			<h1>Orçamento XXX - Assunto do orçamento</h1>
 			<address contenteditable>
 				<p>Jonathan Neal</p>
 				<p>101 E. Chapman Ave<br>Orange, CA 92866</p>
@@ -23,35 +23,43 @@
 			</address>
 			<table class="meta">
 				<tr>
-					<th><span contenteditable>Invoice #</span></th>
-					<td><span contenteditable>101138</span></td>
+					<th><span contenteditable>Orçamento #</span></th>
+					<td><span contenteditable>XXXXXX</span></td>
 				</tr>
 				<tr>
-					<th><span contenteditable>Date</span></th>
-					<td><span contenteditable>January 1, 2012</span></td>
+					<th><span contenteditable>Criado em</span></th>
+					<td><span contenteditable>25/03/2019</span></td>
 				</tr>
 				<tr>
-					<th><span contenteditable>Amount Due</span></th>
-					<td><span id="prefix" contenteditable>$</span><span>600.00</span></td>
+					<th><span contenteditable>Válido até</span></th>
+					<td><span contenteditable>25/04/2019</span></td>
+				</tr>
+				<tr>
+					<th><span contenteditable>Valor Final</span></th>
+					<td>
+						<span id="prefix" contenteditable>R$</span>
+						<span>1.500,00</span>
+					</td>
 				</tr>
 			</table>
 			<table class="inventory">
 				<thead>
 					<tr>
+						<th><span contenteditable>Cód</span></th>
 						<th><span contenteditable>Item</span></th>
-						<th><span contenteditable>Description</span></th>
-						<th><span contenteditable>Rate</span></th>
-						<th><span contenteditable>Quantity</span></th>
-						<th><span contenteditable>Price</span></th>
+						<th><span contenteditable>Descrição</span></th>
+						<th><span contenteditable>Subtotal</span></th>
+						<th><span contenteditable>Qt.</span></th>
+						<th><span contenteditable>Total</span></th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td><a class="cut">-</a><span contenteditable>Front End Consultation</span></td>
-						<td><span contenteditable>Experience Review</span></td>
-						<td><span data-prefix>$</span><span contenteditable>150.00</span></td>
-						<td><span contenteditable>4</span></td>
-						<td><span data-prefix>$</span><span>600.00</span></td>
+						<td><a class="cut">-</a><span contenteditable>Criação de site pessoal</span></td>
+						<td><span contenteditable>Criação de site pessoal para profissionais liberais e pessoa física</span></td>
+						<td><span data-prefix>R$</span><span contenteditable>1.500,00</span></td>
+						<td><span contenteditable>1</span></td>
+						<td><span data-prefix>R$</span><span>1.500,00</span></td>
 					</tr>
 				</tbody>
 			</table>
@@ -59,22 +67,22 @@
 			<table class="balance">
 				<tr>
 					<th><span contenteditable>Total</span></th>
-					<td><span data-prefix>$</span><span>600.00</span></td>
+					<td><span data-prefix>R$</span><span>1.500,00</span></td>
 				</tr>
 				<tr>
-					<th><span contenteditable>Amount Paid</span></th>
-					<td><span data-prefix>$</span><span contenteditable>0.00</span></td>
+					<th><span contenteditable>Créditos e Descontos</span></th>
+					<td><span data-prefix>R$</span><span contenteditable>0.00</span></td>
 				</tr>
 				<tr>
-					<th><span contenteditable>Balance Due</span></th>
-					<td><span data-prefix>$</span><span>600.00</span></td>
+					<th><span contenteditable>Valor Final</span></th>
+					<td><span data-prefix>$</span><span>1.500,00</span></td>
 				</tr>
 			</table>
 		</article>
 		<aside>
-			<h1><span contenteditable>Additional Notes</span></h1>
+			<h1><span contenteditable>Notas e Observações</span></h1>
 			<div contenteditable>
-				<p>A finance charge of 1.5% will be made on unpaid balances after 30 days.</p>
+				<p>Este serviço inclui nosso suporte e manutenção gratuitos por 30 dias.</p>
 			</div>
 		</aside>
 	</body>
