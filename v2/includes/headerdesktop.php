4b57008d3a714992700e7eab94523766efb19c8f
Vila do Site <admin@viladosite.com.br>
2019-03-28 21:46:17 -0300

    Modularização do header desktop

---
+++ v2/includes/headerdesktop.php
@@ -0,0 +1,173 @@
+<!-- HEADER DESKTOP-->
+<header class="header-desktop">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="header-wrap">
+                <form class="form-header" action="" method="POST">
+                    <input class="au-input au-input--xl" type="text" name="search" placeholder="Search for datas &amp; reports..." />
+                    <button class="au-btn--submit" type="submit">
+                        <i class="zmdi zmdi-search"></i>
+                    </button>
+                </form>
+                <div class="header-button">
+                    <div class="noti-wrap">
+                        <div class="noti__item js-item-menu">
+                            <i class="zmdi zmdi-comment-more"></i>
+                            <span class="quantity">1</span>
+                            <div class="mess-dropdown js-dropdown">
+                                <div class="mess__title">
+                                    <p>Você tem 2 novas mensagens</p>
+                                </div>
+                                <div class="mess__item">
+                                    <div class="image img-cir img-40">
+                                        <img src="images/icon/avatar-06.jpg" alt="Michelle Moreno" />
+                                    </div>
+                                    <div class="content">
+                                        <h6>Michelle Moreno</h6>
+                                        <p>Enviou uma foto</p>
+                                        <span class="time">3 min atrás</span>
+                                    </div>
+                                </div>
+                                <div class="mess__item">
+                                    <div class="image img-cir img-40">
+                                        <img src="images/icon/avatar-04.jpg" alt="Diane Myers" />
+                                    </div>
+                                    <div class="content">
+                                        <h6>Diane Myers</h6>
+                                        <p>Agora é possível conversar</p>
+                                        <span class="time">Ontem</span>
+                                    </div>
+                                </div>
+                                <div class="mess__footer">
+                                    <a href="#">Ver todas as mensagens</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="noti__item js-item-menu">
+                            <i class="zmdi zmdi-email"></i>
+                            <span class="quantity">1</span>
+                            <div class="email-dropdown js-dropdown">
+                                <div class="email__title">
+                                    <p>Você tem 3 novos e-mails</p>
+                                </div>
+                                <div class="email__item">
+                                    <div class="image img-cir img-40">
+                                        <img src="images/icon/avatar-06.jpg" alt="Cynthia Harvey" />
+                                    </div>
+                                    <div class="content">
+                                        <p>Bem vindo ao Atencil...</p>
+                                        <span>Diogo Fagundes, 5 min atrás</span>
+                                    </div>
+                                </div>
+                                <div class="email__item">
+                                    <div class="image img-cir img-40">
+                                        <img src="images/icon/avatar-05.jpg" alt="Cynthia Harvey" />
+                                    </div>
+                                    <div class="content">
+                                        <p>Novos recursos disponíveis...</p>
+                                        <span>Bruno Braga, Ontem</span>
+                                    </div>
+                                </div>
+                                <div class="email__item">
+                                    <div class="image img-cir img-40">
+                                        <img src="images/icon/avatar-04.jpg" alt="Cynthia Harvey" />
+                                    </div>
+                                    <div class="content">
+                                        <p>Novo visual implementado...</p>
+                                        <span>Magno Dal Magro, 12/03/2019</span>
+                                    </div>
+                                </div>
+                                <div class="email__footer">
+                                    <a href="#">Ver todos os e-mails</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="noti__item js-item-menu">
+                            <i class="zmdi zmdi-notifications"></i>
+                            <span class="quantity">3</span>
+                            <div class="notifi-dropdown js-dropdown">
+                                <div class="notifi__title">
+                                    <p>Você tem 3 notificações</p>
+                                </div>
+                                <div class="notifi__item">
+                                    <div class="bg-c1 img-cir img-40">
+                                        <i class="zmdi zmdi-email-open"></i>
+                                    </div>
+                                    <div class="content">
+                                        <p>Você tem uma notificação de e-mail</p>
+                                        <span class="date">12/03/2019 15:50</span>
+                                    </div>
+                                </div>
+                                <div class="notifi__item">
+                                    <div class="bg-c2 img-cir img-40">
+                                        <i class="zmdi zmdi-account-box"></i>
+                                    </div>
+                                    <div class="content">
+                                        <p>Sua conta foi suspensa</p>
+                                        <span class="date">12/03/2019 18:25</span>
+                                    </div>
+                                </div>
+                                <div class="notifi__item">
+                                    <div class="bg-c3 img-cir img-40">
+                                        <i class="zmdi zmdi-file-text"></i>
+                                    </div>
+                                    <div class="content">
+                                        <p>Você recebeu um novo arquivo</p>
+                                        <span class="date">10/03/2019 11:32</span>
+                                    </div>
+                                </div>
+                                <div class="notifi__footer">
+                                    <a href="#">Todas as notificações</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="account-wrap">
+                        <div class="account-item clearfix js-item-menu">
+                            <div class="image">
+                                <img src="images/icon/avatar-01.jpg" alt="Diogo Fagundes" />
+                            </div>
+                            <div class="content">
+                                <a class="js-acc-btn" href="#">diogo fagundes</a>
+                            </div>
+                            <div class="account-dropdown js-dropdown">
+                                <div class="info clearfix">
+                                    <div class="image">
+                                        <a href="#">
+                                            <img src="images/icon/avatar-01.jpg" alt="Diogo Fagundes" />
+                                        </a>
+                                    </div>
+                                    <div class="content">
+                                        <h5 class="name">
+                                            <a href="#">diogo fagundes</a>
+                                        </h5>
+                                        <span class="email">admin@viladosite.com.br</span>
+                                    </div>
+                                </div>
+                                <div class="account-dropdown__body">
+                                    <div class="account-dropdown__item">
+                                        <a href="#">
+                                            <i class="zmdi zmdi-account"></i>Conta</a>
+                                    </div>
+                                    <div class="account-dropdown__item">
+                                        <a href="#">
+                                            <i class="zmdi zmdi-settings"></i>Configurações</a>
+                                    </div>
+                                    <div class="account-dropdown__item">
+                                        <a href="#">
+                                            <i class="zmdi zmdi-money-box"></i>Financeiro</a>
+                                    </div>
+                                </div>
+                                <div class="account-dropdown__footer">
+                                    <a href="#">
+                                        <i class="zmdi zmdi-power"></i>Sair</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- HEADER DESKTOP-->
