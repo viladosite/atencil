7b717c1f62ba0bfc62a6ebe63a767f8d613c7b0c
Vila do Site <admin@viladosite.com.br>
2019-03-30 19:36:14 -0300

    Ajustes de caminho de imagens

--- pages/content/at_dashboard.php
+++ pages/content/at_dashboard.php
@@ -280,7 +280,7 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="au-card au-card--no-shadow au-card--no-pad m-b-40">
-                        <div class="au-card-title" style="background-image:url('images/bg-title-01.jpg');">
+                        <div class="au-card-title" style="background-image:url('/images/bg-title-01.jpg');">
                             <div class="bg-overlay bg-overlay--blue"></div>
                             <h3>
                                 <i class="zmdi zmdi-account-calendar"></i>26 April, 2018</h3>
@@ -350,7 +350,7 @@
                 </div>
                 <div class="col-lg-6">
                     <div class="au-card au-card--no-shadow au-card--no-pad m-b-40">
-                        <div class="au-card-title" style="background-image:url('images/bg-title-02.jpg');">
+                        <div class="au-card-title" style="background-image:url('/images/bg-title-02.jpg');">
                             <div class="bg-overlay bg-overlay--blue"></div>
                             <h3>
                                 <i class="zmdi zmdi-comment-text"></i>New Messages</h3>
@@ -373,7 +373,7 @@
                                             <div class="au-message__item-text">
                                                 <div class="avatar-wrap">
                                                     <div class="avatar">
-                                                        <img src="images/icon/avatar-02.jpg" alt="John Smith">
+                                                        <img src="/images/icon/avatar-02.jpg" alt="John Smith">
                                                     </div>
                                                 </div>
                                                 <div class="text">
@@ -391,7 +391,7 @@
                                             <div class="au-message__item-text">
                                                 <div class="avatar-wrap online">
                                                     <div class="avatar">
-                                                        <img src="images/icon/avatar-03.jpg" alt="Nicholas Martinez">
+                                                        <img src="/images/icon/avatar-03.jpg" alt="Nicholas Martinez">
                                                     </div>
                                                 </div>
                                                 <div class="text">
@@ -409,7 +409,7 @@
                                             <div class="au-message__item-text">
                                                 <div class="avatar-wrap online">
                                                     <div class="avatar">
-                                                        <img src="images/icon/avatar-04.jpg" alt="Michelle Sims">
+                                                        <img src="/images/icon/avatar-04.jpg" alt="Michelle Sims">
                                                     </div>
                                                 </div>
                                                 <div class="text">
@@ -427,7 +427,7 @@
                                             <div class="au-message__item-text">
                                                 <div class="avatar-wrap">
                                                     <div class="avatar">
-                                                        <img src="images/icon/avatar-05.jpg" alt="Michelle Sims">
+                                                        <img src="/images/icon/avatar-05.jpg" alt="Michelle Sims">
                                                     </div>
                                                 </div>
                                                 <div class="text">
@@ -445,7 +445,7 @@
                                             <div class="au-message__item-text">
                                                 <div class="avatar-wrap online">
                                                     <div class="avatar">
-                                                        <img src="images/icon/avatar-04.jpg" alt="Michelle Sims">
+                                                        <img src="/images/icon/avatar-04.jpg" alt="Michelle Sims">
                                                     </div>
                                                 </div>
                                                 <div class="text">
@@ -463,7 +463,7 @@
                                             <div class="au-message__item-text">
                                                 <div class="avatar-wrap">
                                                     <div class="avatar">
-                                                        <img src="images/icon/avatar-05.jpg" alt="Michelle Sims">
+                                                        <img src="/images/icon/avatar-05.jpg" alt="Michelle Sims">
                                                     </div>
                                                 </div>
                                                 <div class="text">
@@ -486,7 +486,7 @@
                                     <div class="au-chat-info">
                                         <div class="avatar-wrap online">
                                             <div class="avatar avatar--small">
-                                                <img src="images/icon/avatar-02.jpg" alt="John Smith">
+                                                <img src="/images/icon/avatar-02.jpg" alt="John Smith">
                                             </div>
                                         </div>
                                         <span class="nick">
@@ -499,7 +499,7 @@
                                         <span class="mess-time">12 Min ago</span>
                                         <div class="recei-mess__inner">
                                             <div class="avatar avatar--tiny">
-                                                <img src="images/icon/avatar-02.jpg" alt="John Smith">
+                                                <img src="/images/icon/avatar-02.jpg" alt="John Smith">
                                             </div>
                                             <div class="recei-mess-list">
                                                 <div class="recei-mess">Lorem ipsum dolor sit amet, consectetur adipiscing elit non iaculis</div>
@@ -532,7 +532,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="copyright">
-                        <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+                        <p>Copyright © 2019 Atencil. Todos os direitos reservados. Atencil é um produto <a href="https://colorlib.com">Vila do Site</a> e é baseado no template CoolAdmin da <a href="https://colorlib.com">Colorlib</a>.</p>
                     </div>
                 </div>
             </div>
