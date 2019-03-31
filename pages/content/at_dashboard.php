12ed9b11924455f0f7288bdaea8faaf1aef3042e
Vila do Site <admin@viladosite.com.br>
2019-03-30 22:06:15 -0300

    Separação do footer

--- pages/content/at_dashboard.php
+++ pages/content/at_dashboard.php
@@ -529,13 +529,7 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="copyright">
-                        <p>Copyright © 2019 Atencil. Todos os direitos reservados. Atencil é um produto <a href="https://colorlib.com">Vila do Site</a> e é baseado no template CoolAdmin da <a href="https://colorlib.com">Colorlib</a>.</p>
-                    </div>
-                </div>
-            </div>
+            <?php include '../../includes/at_footer.php'; ?>
         </div>
     </div>
 </div>
