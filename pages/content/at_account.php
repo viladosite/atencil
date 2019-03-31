57641dc372179af572ffa01b0f5a80049eff8e58
Vila do Site <admin@viladosite.com.br>
2019-03-30 22:17:47 -0300

    Inserção do rodapé na página account

--- pages/content/at_account.php
+++ pages/content/at_account.php
@@ -280,13 +280,9 @@
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="copyright">
-                        <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
-                    </div>
-                </div>
-            </div>
+            <!-- Carregamento do rodapé -->
+            <?php include '../includes/at_footer.php'; ?>
+            
         </div>
     </div>
 </div>
