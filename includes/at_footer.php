aba24430acbf92cd7fafea63edb424e0f14c2da0
Vila do Site <admin@viladosite.com.br>
2019-03-30 22:16:21 -0300

    Ajuste de paragrafos no rodapé

--- includes/at_footer.php
+++ includes/at_footer.php
@@ -1,7 +1,8 @@
 <div class="row">
     <div class="col-md-12">
         <div class="copyright">
-            <p>Copyright © 2019 Atencil. Todos os direitos reservados. Atencil é um produto <a href="https://viladosite.com.br">Vila do Site</a> e é baseado no template CoolAdmin da <a href="https://colorlib.com">Colorlib</a>.</p>
+            <p>Copyright © 2019 Atencil. Todos os direitos reservados.</p>
+            <p>Atencil é um produto <a href="https://viladosite.com.br">Vila do Site</a> e é baseado no template CoolAdmin da <a href="https://colorlib.com">Colorlib</a>.</p>
         </div>
     </div>
 </div>
