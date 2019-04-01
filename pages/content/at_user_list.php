d3e33ec02151d6e2ee1de878158508cba43ec072
Vila do Site <admin@viladosite.com.br>
2019-03-31 23:58:29 -0300

    Ajustes nos filtros de user list

--- pages/content/at_user_list.php
+++ pages/content/at_user_list.php
@@ -8,32 +8,36 @@
             <div class="table-data__tool">
                 <div class="table-data__tool-left">
                     <div class="rs-select2--light rs-select2--md">
-                        <select class="js-select2" name="property">
-                            <option selected="selected">All Properties</option>
-                            <option value="">Option 1</option>
-                            <option value="">Option 2</option>
+                        <select class="js-select2" name="grupo">
+                            <option selected="selected">Grupos de usuários</option>
+                            <option value="">Funcionários</option>
+                            <option value="">Gerentes</option>
+                            <option value="">Diretores</option>
                         </select>
                         <div class="dropDownSelect2"></div>
                     </div>
                     <div class="rs-select2--light rs-select2--sm">
-                        <select class="js-select2" name="time">
-                            <option selected="selected">Today</option>
-                            <option value="">3 Days</option>
-                            <option value="">1 Week</option>
+                        <select class="js-select2" name="tempo">
+                            <option selected="selected">Cadastrado em</option>
+                            <option value="">Hoje</option>
+                            <option value="">Últimos 7 dias</option>
+                            <option value="">Últimos 30 dias</option>
+                            <option value="">Últimos 365 Dias</option>
+                            <option value="">Todos os períodos</option>
                         </select>
                         <div class="dropDownSelect2"></div>
                     </div>
                     <button class="au-btn-filter">
-                        <i class="zmdi zmdi-filter-list"></i>filters</button>
+                        <i class="zmdi zmdi-filter-list"></i>filtros</button>
                 </div>
                 <div class="table-data__tool-right">
                     <button class="au-btn au-btn-icon au-btn--green au-btn--small">
-                        <i class="zmdi zmdi-plus"></i>add item</button>
+                        <i class="zmdi zmdi-plus"></i>Cadastrar Usuário</button>
                     <div class="rs-select2--dark rs-select2--sm rs-select2--dark2">
                         <select class="js-select2" name="type">
-                            <option selected="selected">Export</option>
-                            <option value="">Option 1</option>
-                            <option value="">Option 2</option>
+                            <option selected="selected">Exportar</option>
+                            <option value="">Todos os Usuários</option>
+                            <option value="">Apenas os selecionados</option>
                         </select>
                         <div class="dropDownSelect2"></div>
                     </div>
