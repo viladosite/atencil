115bbd0716728c63d14523a32f286c0bb47a0235
Vila do Site <admin@viladosite.com.br>
2019-04-04 04:55:46 -0300

    Ajuste de core

---
+++ includes/at_core.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// Carrega as configurações para funcionamento do sistema
+require __DIR__ . '/../config.php';
+
+// Faz a conexão com o banco
+require __DIR__ . '/../auth/at_connect.php';
+
+// Carrega as funções para funcionamento do sistema
+require __DIR__ . '/../functions/functions.php';
+
+
+?>
