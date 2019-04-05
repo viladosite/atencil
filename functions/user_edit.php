522e6f505120876d7206b71d8f23204e1bf670d3
Vila do Site <admin@viladosite.com.br>
2019-04-05 03:18:25 -0300

    Ajustes do edit user

--- functions/user_edit.php
+++ functions/user_edit.php
@@ -4,50 +4,46 @@
 require __DIR__ . '/../includes/at_core.php';
 
 
+
 // Monta os dados enviados nos campos em variáveis e as prepara para uso no mysql
 $formemail = $_POST['email'];
-// $formemail = mysqli_real_escape_string($mysql, $_POST['email']);
-
-$formsenhaatual = $_POST['senhaatual'];
-// $formsenhaatual = mysqli_real_escape_string($mysql, sha1($_POST['senhaatual']));
-
-$formsenhanova = $_POST['senhanova'];
-// $formsenhanova = mysqli_real_escape_string($mysql, $_POST['senhanova']);
-
+$formsenhaatual = sha1($_POST['senhaatual']);
+$formsenhanova = sha1($_POST['senhanova']);
 $useridatual = $_SESSION['UserID'];
-// $useridatual = mysqli_real_escape_string($mysql, $_SESSION['UserID']);
 
 
-
-// Caso o usuário não preencha o email, mantem o email do usuário logado
-if (empty( $_POST['email'] )) { $formemail = $_SESSION['UserEmail']; }
-// Caso o usuário forneça uma senha nova, informa ela para update no banco
-if (!empty( $_POST['senhanova'] )) { $formsenhanova = ", userpass= '$formsenhanova'"; }
+// Condicionais de verificação de preenchimento de campos
+if ( empty( $formemail ) ) { $formemail = $_SESSION['UserEmail']; }
+if ( empty( $formsenhanova ) ) { $formsenhanova = $formsenhatual; }
 
 
-
-
-// Checa a conexão
+// Checagem de conexão
 if (!$mysql) { die("A Conexão Falhou: " . mysqli_connect_error()); }
 
-// Verifica na tabela se encontrou o usuário com id logado e senha correspondente à fornecida
-$selectuser = "SELECT * FROM at_users WHERE (userid = '$useridatual') AND (userpass = '$formsenhaatual')";
 
-// Verifica se é possível encontrar o usuário
-$checauser = mysqli_query($mysql, $selectuser);
+// Queries de checagem dos dados
+$usersql = " SELECT * FROM at_users WHERE `userpass`='$formsenhatual' AND `userid` = '$useridatual' ";
 
-// Monta a query de update dos dados
-$update = "UPDATE at_users SET usermail = '$formemail' '$formsenhanova' WHERE userid = '$useridatual' ";
+
+// Execução de verificação de dados
+$checauser = mysqli_query($mysql, $usersql);
+
+
+// Monta as queries de update dos dados
+$updatemail = " UPDATE at_users SET usermail = '$formemail' WHERE userid = '$useridatual' ";
+$updatesenha = " UPDATE at_users SET userpass = '$formsenhanova' WHERE userid = '$useridatual' ";
+
 
 // Checa quantas linhas de resultado encontrou na checagem
-$resultado = mysqli_num_rows($checauser);
+$resultuser = mysqli_num_rows($checauser);
 
 
 // Define o comportamento caso encontre o usuário e caso não encontre
-if ($resultado = 1) {
+if ($resultuser = 1) {
 
 	// Executa a query de update dos dados
-	mysqli_query($mysql, $update);
+	mysqli_query($mysql, $updatemail);
+	if ( $formsenhanova != $formsenhaatual ) { mysqli_query($mysql, $updatesenha); };
 	
 	// Mostra o aviso para relogar
 	echo '<script type="text/javascript"> 
@@ -56,6 +52,9 @@ if ($resultado = 1) {
 
 	// Destroi a sessão do usuário para forçar o relogin
 	session_destroy();
+
+	// Encerra a conexão
+	mysqli_close($mysql);
 
     // Direciona para a tela de login
     header("Location: /index.php");
@@ -70,11 +69,9 @@ if ($resultado = 1) {
     // Direciona de volta para a página da conta
     header("Location: pages/account.php");
 
+    // Encerra a conexão
+	mysqli_close($mysql);
+
 }
-
-
-// Encerra a conexão
-mysqli_close($mysql);
-
 
 ?>
