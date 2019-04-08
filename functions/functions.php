d196c8d574236bd219e0639291536ae4944ba823
Vila do Site <admin@viladosite.com.br>
2019-04-08 12:51:35 -0300

    Remoção de quebra de linha

--- functions/functions.php
+++ functions/functions.php
@@ -76,8 +76,8 @@ function badge_edit($permission) {
 
 // Função para checagem das permissões de acesso dos recursos específicos da empresa
 function badges($permview, $permedit) {
-	if(perm_check($permview) == true) {echo '<span class="badge badge-success permtag">Ver</span><br>';}
-	if(perm_check($permedit) == true) {echo '<span class="badge badge-warning permtag">Editar</span><br>';}	
+	if(perm_check($permview) == true) {echo '<span class="badge badge-success permtag">Ver</span>';}
+	if(perm_check($permedit) == true) {echo '<span class="badge badge-warning permtag">Editar</span>';}	
 }
 
 
