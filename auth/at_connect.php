7cb626ae95a3628f34f89e39532af675984b286c
Vila do Site <admin@viladosite.com.br>
2019-04-10 13:27:51 -0300

    Ajuste de variável de conexão

--- auth/at_connect.php
+++ auth/at_connect.php
@@ -1,7 +1,7 @@
 <?php
 
 // Tenta se conectar ao servidor MySQL
-$mysql = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname) or trigger_error(mysql_error());
+global $mysql = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname) or trigger_error(mysql_error());
 
 // Tenta se conectar ao banco de dados MySQL prédefinido nas configurações
 mysqli_select_db($mysql, $dbname) or trigger_error(mysql_error());
