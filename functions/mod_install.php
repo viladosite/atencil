26ad29cace46c5160b61b4f63db320ba3fdc6c67
Vila do Site <admin@viladosite.com.br>
2019-11-06 14:47:40 -0300

    Inclusão do arquivo de função de instalação de módulo

---
+++ functions/mod_install.php
@@ -0,0 +1,76 @@
+<?php
+
+// Carrega as configurações, funções e elementos base para funcionamento do sistema
+require __DIR__ . '/../includes/at_core.php';
+
+
+// Variáveis com dados do formulário
+$formpnome = mysqli_real_escape_string($mysql, $_POST['pnome']);
+$formunome = mysqli_real_escape_string($mysql, $_POST['unome']);
+$formuser = mysqli_real_escape_string($mysql, $_POST['user']);
+$formemail = mysqli_real_escape_string($mysql, $_POST['email']);
+$formsenha = sha1($_POST['senha']);
+$usercompanylvl = mysqli_real_escape_string($mysql, $_POST['permissao']);
+
+
+// Variáveis nativas do usuário
+$useridatual = $_SESSION['UserID'];
+$usercompany = $_SESSION['UserCompany'];
+$userperm = 1;
+$userregstatus = 1;
+$regdate = date("Y-m-d H:i:s");
+
+
+
+// Condicionais de verificação de preenchimento de campos
+if ( empty($formpnome) or empty($formunome)	or empty($formuser)	or empty($formemail) or empty($formsenha) ) {
+	
+	// Retorna o erro caso algum campo tenha sido deixado em branco
+	echo '<script type="text/javascript">alert("Você não preencheu algum campo. Preencha todos os campos para continuar.");history.go(-1);</script>';
+
+} else {
+
+	// Checagem de conexão
+	if (!$mysql) { die("A Conexão Falhou: " . mysqli_connect_error()); }
+
+
+	// Queries de inserção dos dados
+	$usersql = "
+		INSERT INTO at_users (userfname, userlname, userlogin, userpass, usermail, userpermlvl, usercomp, usercomplvl, userstatus, userregdate)
+		VALUES ('$formpnome', '$formunome', '$formuser', '$formsenha', '$formemail', '$userperm', '$usercompany', '$usercompanylvl', '$userregstatus', '$regdate');
+	";
+
+	// Execução de inserção de dados
+	$criauser = mysqli_query($mysql, $usersql);
+
+	// Mostra mensagem de confirmação caso o registro funcione
+	if ($criauser) {
+
+		// Mostra o aviso para relogar
+		echo '<script type="text/javascript"> 
+	    window.alert("O usuário foi criado com sucesso.");
+	    </script>';
+
+	    // Direciona para a tela de login
+	    header("Location: /pages/user_list.php");
+
+	    // Encerra a conexão
+		mysqli_close($mysql);
+
+	} else {
+
+		// Mostra o aviso para relogar
+		echo '<script type="text/javascript"> 
+	    window.alert("Houve um erro ao cadastrar o usuário.");
+	    </script>';
+
+	    // Direciona para a tela de login
+	    header("Location: /pages/user_new.php");
+
+	    // Encerra a conexão
+		mysqli_close($mysql);
+	}
+
+}
+
+?>
