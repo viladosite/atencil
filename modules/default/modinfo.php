f27835fa56b5d054aabe3da725829fce9b4bb5ae
Vila do Site <admin@viladosite.com.br>
2019-11-07 10:56:48 -0300

    Separação do módulo default

--- modules/default/modinfo.php
+++
@@ -1,8 +0,0 @@
-{
-    "name" : "Default Test Module",
-    "category" : "Default",
-    "author" : "Vila do Site",
-    "authorurl": "http://viladosite.com.br",
-    "modlogo": "logo.png",
-    "modpath": "defaultmod"
-}
