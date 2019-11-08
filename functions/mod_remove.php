0644e426353c9b4b81fc4499f4d8c1152b4e1c6c
Vila do Site <admin@viladosite.com.br>
2019-11-08 10:29:58 -0300

    Ajuste da remoção de arquivos

--- functions/mod_remove.php
+++ functions/mod_remove.php
@@ -11,9 +11,6 @@ $modformdir = $_POST['modtoremdir'];
 if (!$mysql) { die("A Conexão Falhou: " . mysqli_connect_error()); }
 
 
-// Prepara a remoção da pasta e arquivos
-$modremdir = delete_files('$home_dir . $mods_dir . "/" . $modformdir');
-
 // Prepara a query de remoção do banco
 $modremquery = " DELETE FROM at_modules WHERE modid = '$modformid' ";
 $modremdb = mysqli_query($mysql, $modremquery);
@@ -22,6 +19,9 @@ $modremdb = mysqli_query($mysql, $modremquery);
 // Executa a remoção do banco e dos arquivos
 if ($modrembd = true) {
 	
+	// Remove os arquivos e a pasta do módulo
+	delete_files('$home_dir . $mods_dir . "/" . $modformdir');
+
 	// Redirect back to the instalation page
 	header("Location: ../pages/mod_list.php");
 
