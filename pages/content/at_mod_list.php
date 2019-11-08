0f247dbf12644322f2df6e4180888db033432a04
Vila do Site <admin@viladosite.com.br>
2019-11-08 09:26:28 -0300

    Correção do botão do modal

--- pages/content/at_mod_list.php
+++ pages/content/at_mod_list.php
@@ -117,7 +117,7 @@
                                         <i class="zmdi zmdi-refresh"></i>
                                     </button>
 
-                                    <button class="item" data-toggle="modal" data-target="#confirmar" title="Remover" name="function" value='delete' type="button">
+                                    <button class="item" data-toggle="modal" data-target="#confirmar" title="Remover" name="function" value='delete' type="submit">
                                         <i class="zmdi zmdi-delete"></i>
                                     </button>
 
