216b02cb8f5fb1d931d0dd74f874f00a7714646b
Vila do Site <admin@viladosite.com.br>
2019-11-08 10:20:08 -0300

    Correção do método do mod remove

--- includes/at_modal_moddel_conf.php
+++ includes/at_modal_moddel_conf.php
@@ -23,9 +23,9 @@
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
                 
-                <form action="/functions/mod_remove.php" style="display: inline;">
-                    <input type="hidden" id="modtoremid" name="modtoremid" value='<?php echo $mod['modid']; ?>'>
-                    <input type="hidden" id="modtoremdir" name="modtoremdir" value='<?php echo $mod['modpath']; ?>'>
+                <form action="/functions/mod_remove.php" style="display: inline;" method="post">
+                    <input type="hidden" id="modtoremid" name="modtoremid" value="<?php echo $mod['modid']; ?>">
+                    <input type="hidden" id="modtoremdir" name="modtoremdir" value="<?php echo $mod['modpath']; ?>">
                     <button type="submit" class="btn btn-primary">Remover</button>
                 </form>
 
