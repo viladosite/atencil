740ac4d95c177e57b6dfbd530aad3c574fa06851
Vila do Site <admin@viladosite.com.br>
2019-11-17 04:44:12 -0300

    Criação dos arquivos de instalação

---
+++ install/install_step1.php
@@ -0,0 +1,56 @@
+<div class="page-content--bge5">
+    <div class="container">
+        <div class="login-wrap">
+            <div class="login-content">
+                <div class="login-logo">
+                    <a href="index.php">
+                        <img src="../../images/logo_atencil_horiz.png" alt="Atencil">
+                    </a>
+                </div>
+                <div class="login-form">
+                    <p>
+                        Vamos instalar o ATENCIL. Tudo pronto?
+                    </p>
+                    <br><br>
+
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label>Nome de Usuário</label>
+                            <input class="au-input au-input--full" type="text" name="fielduser" placeholder="Nome de Usuário">
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input class="au-input au-input--full" type="email" name="fieldemail" placeholder="Digite seu Email">
+                        </div>
+                        <div class="form-group">
+                            <label>Senha</label>
+                            <input class="au-input au-input--full" type="password" name="fieldpass" placeholder="Digite sua senha">
+                        </div>
+                        <div class="login-checkbox">
+                            <label>
+                                <input type="checkbox" name="aggree">Concordo com os <a href="#" data-toggle="modal" data-target="#modalterms">termos e condições</a>
+                            </label>
+                        </div>
+                        <button class="au-btn au-btn--block au-btn--blue m-b-20" type="submit">Cadastrar</button>
+                        
+                        <!--
+                        <div class="social-login-content">
+                            <div class="social-button">
+                                <button class="au-btn au-btn--block au-btn--blue m-b-20">register with facebook</button>
+                                <button class="au-btn au-btn--block au-btn--blue2">register with twitter</button>
+                            </div>
+                        </div>
+                        -->
+
+                    </form>
+                    <div class="register-link">
+                        <p>
+                            Já possui uma conta?
+                            <a href='../../index.php'>Acessar</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
