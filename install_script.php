c96083fde48646c331cf208aa73c29ff5cf2cfa1
Vila do Site <admin@viladosite.com.br>
2019-11-25 13:45:50 -0300

    Remoção dos drop trigger
    Segundo a documentação do mysql ao dropar uma tabela os triggers vão junto e como a instalação já dropa as tabelas logo de cara, não seria necessário o drop trigger.

--- install_script.php
+++ install_script.php
@@ -93,7 +93,7 @@
 
 
         // Apaga os triggers caso existam
-        $triggerdrops = "DROP TRIGGER IF EXISTS `client_group_id1block`; DROP TRIGGER IF EXISTS `company_id1block`; DROP TRIGGER IF EXISTS `user_group_id1block`; DROP TRIGGER IF EXISTS `user_id1block`;";
+        // $triggerdrops = "DROP TRIGGER IF EXISTS `client_group_id1block`; DROP TRIGGER IF EXISTS `company_id1block`; DROP TRIGGER IF EXISTS `user_group_id1block`; DROP TRIGGER IF EXISTS `user_id1block`;";
 
         // Monta os triggers para as tabelas
         $trigger1 = "CREATE TRIGGER `client_group_id1block` BEFORE DELETE ON `at_clientgroups` FOR EACH ROW IF old.cligrid IN (1) THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Este grupo não pode ser removido!!'; END IF";
