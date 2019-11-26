e6de2744b123532a12d5203d004003d221adb9fc
Vila do Site <admin@viladosite.com.br>
2019-11-25 21:35:39 -0300

    Ajuste do layout de user create

--- pages/content/at_user_new.php
+++ pages/content/at_user_new.php
@@ -5,101 +5,14 @@
 
             <h3 class="title-5 m-b-35">Cadastrar Usuário</h3>
 
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="card">
-                        <div class="card-header">
-                            <strong>Atenção:</strong>
-                        </div>
-                        <div class="card-body">
-                            <p>Este usuário será cadastrado como um usuário vinculado à sua empresa e poderá ver detalhes de sua empresa como </p>
-                            <p>Em breve</p>
-                        </div>
-                    </div>
-                </div>
+            <form action="../functions/user_create.php" method="post">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="card">
+                            
+                            <div class="card-header">Grupo e status</div>
 
-                <div class="col-lg-8">
-                    
-                    <div class="card">
-                        <div class="card-header">Dados do Usuário</div>
-                        <div class="card-body card-block">
-                            <form action="../functions/user_create.php" method="post">
-
-                                <div class="row form-group">
-                                    <div class="col col-md-3">
-                                        <label for="group" class=" form-control-label">Grupo</label>
-                                    </div>
-                                    <div class="col-12 col-md-9">
-                                        <select name="group" id="group" class="form-control">
-                                            <?php
-                                            // Monta os parâmetros da query
-                                            $sqlgr = "SELECT * FROM `at_usergroups` WHERE (`usergroupcomp` = '".$_SESSION['UserCompany'] ."')";
-
-                                            // Monta a query para execução
-                                            $usergrs = mysqli_query($mysql, $sqlgr);
-
-                                            // Executa o loop com a query
-                                            foreach ($usergrs as $usergr) { ?>
-                                            
-                                            <option value="<?php echo $usergr['usergroupid']; ?>">
-                                                <?php echo $usergr['usergroupname']; ?>
-                                            </option>
-                                            
-                                            <?php } ?>
-
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">Primeiro Nome</div>
-                                        <input type="text" id="pnome" name="pnome" class="form-control">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-user"></i>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">Último Nome</div>
-                                        <input type="text" id="unome" name="unome" class="form-control">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-user"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                                
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">Nome de Usuário</div>
-                                        <input type="text" id="user" name="user" class="form-control">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-user"></i>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">Email</div>
-                                        <input type="email" id="email" name="email" class="form-control">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-envelope"></i>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">Senha</div>
-                                        <input type="password" id="senha" name="senha" class="form-control">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-asterisk"></i>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div class="card-body">
 
                                 <div class="form-group">
                                     <label class="switch switch-3d switch-success mr-3">
@@ -110,18 +23,101 @@
                                     <label for="status">Ativar este usuário?</label>
                                 </div>
 
+                                <div class="form-group">
+                                    <label for="group" class=" form-control-label">Grupo</label>
+                                    <select name="group" id="group" class="form-control">
+                                        <?php
+                                        // Monta os parâmetros da query
+                                        $sqlgr = "SELECT * FROM `at_usergroups` WHERE (`usergroupcomp` = '".$_SESSION['UserCompany'] ."')";
+
+                                        // Monta a query para execução
+                                        $usergrs = mysqli_query($mysql, $sqlgr);
+
+                                        // Executa o loop com a query
+                                        foreach ($usergrs as $usergr) { ?>
+                                        
+                                        <option value="<?php echo $usergr['usergroupid']; ?>">
+                                            <?php echo $usergr['usergroupname']; ?>
+                                        </option>
+                                        
+                                        <?php } ?>
+
+                                    </select>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-8">
+                        
+                        <div class="card">
+                            <div class="card-header">Dados Obrigatórios</div>
+                            <div class="card-body card-block">
+
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon col-md-4">Primeiro Nome</div>
+                                        <input type="text" id="pnome" name="pnome" class="form-control">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-user"></i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon col-md-4">Último Nome</div>
+                                        <input type="text" id="unome" name="unome" class="form-control">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-user"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon col-md-4">Nome de Usuário</div>
+                                        <input type="text" id="user" name="user" class="form-control">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-user"></i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon col-md-4">Email</div>
+                                        <input type="email" id="email" name="email" class="form-control">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-envelope"></i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon col-md-4">Senha</div>
+                                        <input type="password" id="senha" name="senha" class="form-control">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-asterisk"></i>
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <div class="form-actions form-group">
                                     <button type="submit" class="btn btn-primary btn-sm">
                                         Cadastrar
                                     </button>
                                 </div>
 
-                            </form>
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
