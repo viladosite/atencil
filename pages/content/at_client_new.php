33cd7a1ad78bbe088750d6aa6b055d031fef1891
Vila do Site <admin@viladosite.com.br>
2019-11-25 23:40:31 -0300

    Inclusão de campos no create client

--- pages/content/at_client_new.php
+++ pages/content/at_client_new.php
@@ -1,60 +1,67 @@
 <!-- MAIN CONTENT-->
+<?php
+    // Obter DDD padrão configurado para a empresa do usuário
+    // $_SESSION['UserCompany']
+    // $dddquery = "SELECT `configvalue1` FROM `at_config` WHERE (`configcomp` = '".$_SESSION['UserCompany'] ."') AND (`configname` = `DDD`)";
+    // $ddd = mysqli_query($mysql, $dddquery);
+    $ddd = '21';
+    
+?>
 <div class="main-content">
     <div class="section__content section__content--p30">
         <div class="container-fluid">
 
             <h3 class="title-5 m-b-35">Cadastrar Usuário</h3>
 
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="card">
-                        <div class="card-header">
-                            <strong>Atenção:</strong>
-                        </div>
-                        <div class="card-body">
-                            <p>Este cliente será cadastrado como um cliente vinculado à sua empresa e seus funcionários ou usuários podem ter acesso a dados dele como:</p>
-                            <p>Em breve</p>
+            <form action="../functions/client_create.php" method="post">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="card">
+                            
+                            <div class="card-header">Grupo e Status</div>
+
+                            <div class="card-body">
+
+                                <div class="form-group">
+
+                                    <label for="status" class=" form-control-label">Grupo</label>
+
+                                    <select name="group" id="group" class="form-control">
+
+                                        <?php
+                                        // Monta os parâmetros da query
+                                        $sqlgr = "SELECT * FROM `at_clientgroups` WHERE (`cligrcompany` = '".$_SESSION['UserCompany'] ."')";
+
+                                        // Monta a query para execução
+                                        $usergrs = mysqli_query($mysql, $sqlgr);
+
+                                        // Executa o loop com a query
+                                        foreach ($usergrs as $usergr) { ?>
+                                        
+                                        <option value="<?php echo $usergr['cligrid']; ?>">
+                                            <?php echo $usergr['cligrname']; ?>
+                                        </option>
+                                        
+                                        <?php } ?>
+
+                                    </select>
+
+                                </div>
+
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="col-lg-8">
-                    
-                    <div class="card">
-                        <div class="card-header">Dados do Cliente</div>
-                        <div class="card-body card-block">
-                            <form action="../functions/client_create.php" method="post">
-
-                            <div class="row form-group">
-                                    <div class="col col-md-3">
-                                        <label for="status" class=" form-control-label">Grupo</label>
-                                    </div>
-                                    <div class="col-12 col-md-9">
-                                        <select name="group" id="group" class="form-control">
-
-                                            <?php
-                                            // Monta os parâmetros da query
-                                            $sqlgr = "SELECT * FROM `at_clientgroups` WHERE (`cligrcompany` = '".$_SESSION['UserCompany'] ."')";
-
-                                            // Monta a query para execução
-                                            $usergrs = mysqli_query($mysql, $sqlgr);
-
-                                            // Executa o loop com a query
-                                            foreach ($usergrs as $usergr) { ?>
-                                            
-                                            <option value="<?php echo $usergr['cligrid']; ?>">
-                                                <?php echo $usergr['cligrname']; ?>
-                                            </option>
-                                            
-                                            <?php } ?>
-
-                                        </select>
-                                    </div>
-                                </div>
+                    <div class="col-lg-8">
+                        
+                        <div class="card">
+                            <div class="card-header">Dados do Cliente</div>
+                            
+                            <div class="card-body card-block">
 
                                 <div class="form-group">
                                     <div class="input-group">
-                                        <div class="input-group-addon">Primeiro Nome</div>
+                                        <div class="input-group-addon col-md-4">Primeiro Nome</div>
                                         <input type="text" id="pnome" name="pnome" class="form-control">
                                         <div class="input-group-addon">
                                             <i class="fa fa-user"></i>
@@ -64,7 +71,7 @@
 
                                 <div class="form-group">
                                     <div class="input-group">
-                                        <div class="input-group-addon">Último Nome</div>
+                                        <div class="input-group-addon col-md-4">Último Nome</div>
                                         <input type="text" id="unome" name="unome" class="form-control">
                                         <div class="input-group-addon">
                                             <i class="fa fa-user"></i>
@@ -74,11 +81,108 @@
                                 
                                 <div class="form-group">
                                     <div class="input-group">
-                                        <div class="input-group-addon">Email</div>
+                                        <div class="input-group-addon col-md-4">Email</div>
                                         <input type="email" id="email" name="email" class="form-control">
                                         <div class="input-group-addon">
                                             <i class="fa fa-envelope"></i>
                                         </div>
+                                    </div>
+                                </div>
+
+                                <span>Dados Opcionais</span>
+                                <hr>
+
+                                <div class="row form-group">
+                                    <div class="col-md-2 col-sm-6 d-flex justify-content-center">
+                                        <span>País</span>
+                                    </div>
+                                    <div class="col-md-2 col-sm-6 d-flex justify-content-center">
+                                        <span>DDD</span>
+                                    </div>
+                                    <div class="col-md-8 col-sm-12 d-flex justify-content-center">
+                                        <span>Telefone</span>
+                                    </div>
+                                </div>
+                                    
+                                <div class="row form-group">
+                                    <div class="col-md-2 col-sm-6">
+                                        <input type="text" class="form-control" value="+55">
+                                    </div>
+                                    <div class="col-md-2 col-sm-6">
+                                        <input type="text" value="<?php echo $ddd; ?>"  class="form-control">
+                                    </div>
+                                    <div class="col-md-8 col-sm-12">
+                                        <input type="tel" pattern="[0-9]*" placeholder="Telefone 1" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="row form-group">
+                                    <div class="col-md-2 col-sm-6">
+                                        <input type="text" class="form-control" value="+55">
+                                    </div>
+                                    <div class="col-md-2 col-sm-6">
+                                        <input type="text" value="<?php echo $ddd; ?>"  class="form-control">
+                                    </div>
+                                    <div class="col-md-8 col-sm-12">
+                                        <input type="tel" pattern="[0-9]*" placeholder="Telefone 2" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="row form-group">
+                                    <div class="col-md-2 col-sm-6">
+                                        <input type="text" class="form-control" value="+55">
+                                    </div>
+                                    <div class="col-md-2 col-sm-6">
+                                        <input type="text" value="<?php echo $ddd; ?>"  class="form-control">
+                                    </div>
+                                    <div class="col-md-8 col-sm-12">
+                                        <input type="tel" pattern="[0-9]*" placeholder="Telefone 3" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="row form-group">
+                                    <div class="col-12"><span>Whatsapp</span></div>
+                                </div>
+                                
+                                <div class="row form-group">
+                                    <div class="col-md-2 col-sm-6">
+                                        <input type="text" class="form-control" value="+55">
+                                    </div>
+                                    <div class="col-md-2 col-sm-6">
+                                        <input type="text" value="<?php echo $ddd; ?>"  class="form-control">
+                                    </div>
+                                    <div class="col-md-8 col-sm-12">
+                                        <input type="text" placeholder="Whatsapp" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="row form-group">
+                                    <div class="col-12"><span>Data de nascimento</span></div>
+                                </div>
+
+                                <div class="row form-group">
+                                    <div class="col-3">
+                                        <input type="text" class="form-control" placeholder="Dia" maxlength="2">
+                                    </div>
+                                    <div class="col-3">
+                                        <select name="select" id="select" class="form-control">
+                                            <option value="0">Mês</option>
+                                            <option value="1">01 - Jan</option>
+                                            <option value="2">02 - Fev</option>
+                                            <option value="3">03 - Mar</option>
+                                            <option value="4">04 - Abr</option>
+                                            <option value="5">05 - Mai</option>
+                                            <option value="6">06 - Jun</option>
+                                            <option value="7">07 - Jul</option>
+                                            <option value="8">08 - Ago</option>
+                                            <option value="9">09 - Set</option>
+                                            <option value="10">10 - Out</option>
+                                            <option value="11">11 - Nov</option>
+                                            <option value="12">12 - Dez</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-6">
+                                        <input type="text" class="form-control" placeholder="Ano (4 digitos)" maxlength="4">
                                     </div>
                                 </div>
 
@@ -87,13 +191,13 @@
                                         Cadastrar
                                     </button>
                                 </div>
-
-                            </form>
+                                
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
