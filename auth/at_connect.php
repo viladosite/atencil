8705a401a8b588d53f2fcad7b74111fc4f1c4150
bruckbmrj <bruckbmrj@hotmail.com>
2019-11-29 13:17:55 -0300

    testar pdo na instalação

--- auth/at_connect.php
+++ auth/at_connect.php
@@ -1,8 +1,29 @@
 <?php
 
-// Tenta se conectar ao servidor MySQL
+class Database{
+	// Tenta se conectar ao servidor MySQL
+	public $conn;
 
-if ( defined('DB_HOST') && defined('DB_USER') && defined('DB_PASS') && defined('DB_NAME') ){
+	// get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Erro na conexão: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+
+}
+
+
+
+/*if ( defined('DB_HOST') && defined('DB_USER') && defined('DB_PASS') && defined('DB_NAME') ){
     $mysql = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     mysqli_select_db($mysql, DB_NAME);
 } else {
@@ -13,6 +34,6 @@ if ( defined('DB_HOST') && defined('DB_USER') && defined('DB_PASS') && defined('
 mysqli_query($mysql, "SET NAMES 'utf8'");
 mysqli_query($mysql, 'SET character_set_connection=utf8');
 mysqli_query($mysql, 'SET character_set_client=utf8');
-mysqli_query($mysql, 'SET character_set_results=utf8');
+mysqli_query($mysql, 'SET character_set_results=utf8');*/
 
 ?>
--- auth/at_login.php
+++ auth/at_login.php
@@ -1,5 +1,4 @@
 <?php
-
 // Carrega as configurações para funcionamento do sistema
 require __DIR__ . '/../config.php';
 
@@ -9,42 +8,90 @@ require __DIR__ . '/at_connect.php';
 // Carrega as funções para funcionamento do sistema
 require __DIR__ . '/../functions/functions.php';
 
+//Monta as variáveis para os campos do form
+    //$user = $_POST['fielduser'];
+    $pass = isset($_POST['fieldpass']) ? $_POST['fieldpass'] : '';
+    $email = isset($_POST['fieldemail']) ? $_POST['fieldemail'] : '';
+    // $name = $_POST['fname'];
+    // $name = $_POST['lname'];
 
-
-// Verifica se houve POST e se o usuário ou a senha é(são) vazio(s)
+    // Verifica se houve POST e se o usuário ou a senha é(são) vazio(s)
     if (!empty($_POST) AND (empty($_POST['fieldemail']) OR empty($_POST['fieldpass']))) {
         header("Location: ../index.php"); exit;
     }
-      
 
+class Login{
+    private $conn;
+    private $table_name = "at_users";
 
-//Monta as variáveis para os campos do form
-$user = $_POST['fielduser'];
-$pass = $_POST['fieldpass'];
-$email = $_POST['fieldemail'];
-// $name = $_POST['fname'];
-// $name = $_POST['lname'];
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    function readLogin(){
+        $query = "SELECT * FROM". $this->table_name ." WHERE ( usermail = :email) AND (userpass = :password) AND (userstatus = 'ativo') LIMIT 1";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', sha1($pass));
+     
+        // execute query
+        $stmt->execute();
+     
+        // Salva os dados encontados na variável $users
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if (count($users) <= 0)
+        {
+            echo '<script type="text/javascript"> window.alert("Dados incorretos, tente novamente ou procure o administrador do sistema.");
+            window.history.back();</script>';
+        } else{
+            // Se a sessão não existir, inicia uma
+            if (!isset($_SESSION)) session_start();
+          
+            // Salva os dados encontrados na sessão
+            $_SESSION['UserID'] = $users['userid'];
+            $_SESSION['UserFname'] = $users['userfname'];
+            $_SESSION['UserLname'] = $users['userlname'];
+            $_SESSION['UserLogin'] = $users['userlogin'];
+            $_SESSION['UserEmail'] = $users['usermail'];
+            $_SESSION['UserCompany'] = $users['usercomp'];
+            $_SESSION['UserGroup'] = $users['usergroup'];
+            $_SESSION['UserActive'] = $users['userstatus'];
+            $_SESSION['UserRegDate'] = $users['userregdate'];
+          
+            // Redireciona o visitante
+            header("Location: ../pages/dashboard.php"); exit;
+
+        }
+
+    }    
+
+}    
 
 
 // Query que da um select no banco e se encontrar algo diferente de 1 como resultado, retorna erro
-	$sql = "SELECT * FROM `at_users` WHERE (`usermail` = '".$email ."') AND (`userpass` = '". sha1($pass) ."') AND (`userstatus` = 'ativo') LIMIT 1";
+	/*$sql = "SELECT * FROM `at_users` WHERE (`usermail` = '".$email ."') AND (`userpass` = '". sha1($pass) ."') AND (`userstatus` = 'ativo') LIMIT 1";
 
-    $query = mysqli_query($mysql, $sql);
+    $query = mysqli_query($mysql, $sql);*/
 	
-    if (mysqli_num_rows($query) != 1) {
+    //if (mysqli_num_rows($query) != 1) {
 
 // Caso a resposta do select seja diferente de 1, exibe a mensagem de erro
 // Este caso se aplica quando os dados são inválidos, o usuário não foi encontrado ou até mesmo por algum erro foram encontrados mais de 1 login
         
-	echo '<script type="text/javascript"> 
+	/*echo '<script type="text/javascript"> 
     window.alert("Dados incorretos, tente novamente ou procure o administrador do sistema.");
     window.history.back();
-    </script>';
+    </script>';*/
 
 
 // Caso a resposta do select seja 1, o sistema encontrou o usuário, portanto salva os dados encontrados na sessão e redireciona o visitante para a devida página
 
-    } else {
+    /*} else {
 
 		// Salva os dados encontados na variável $resultado
         $resultado = mysqli_fetch_assoc($query);
@@ -65,6 +112,6 @@ $email = $_POST['fieldemail'];
       
         // Redireciona o visitante
         header("Location: ../pages/dashboard.php"); exit;
-    }
+    }*/
 
 ?>
