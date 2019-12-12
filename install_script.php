c4b3d677811c0730ca2849e0c2ff4383b873c0dc
Vila do Site <admin@viladosite.com.br>
2019-12-12 11:49:16 -0300

    Remoção do drop trigger

--- install_script.php
+++ install_script.php
@@ -93,7 +93,7 @@
 
 
         // Apaga os triggers caso existam
-        $triggerdrops = "DROP TRIGGER IF EXISTS client_group_id1block; DROP TRIGGER IF EXISTS company_id1block; DROP TRIGGER IF EXISTS user_group_id1block; DROP TRIGGER IF EXISTS user_id1block;";
+        // $triggerdrops = "DROP TRIGGER IF EXISTS client_group_id1block; DROP TRIGGER IF EXISTS company_id1block; DROP TRIGGER IF EXISTS user_group_id1block; DROP TRIGGER IF EXISTS user_id1block;";
 
         // Monta os triggers para as tabelas
         $trigger1 = "CREATE TRIGGER `client_group_id1block` BEFORE DELETE ON `at_clientgroups` FOR EACH ROW IF old.cligrid IN (1) THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Este grupo não pode ser removido!!'; END IF";
@@ -105,7 +105,7 @@
         $trigger4 = "CREATE TRIGGER `user_id1block` BEFORE DELETE ON `at_users` FOR EACH ROW IF old.userid IN (1) THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Este usuário é o Superadmin e não pode ser removido!!'; END IF";
 
         // Agrupa os triggers para inclusão no banco
-        $triggers = $triggerdrops . ";" . $trigger1 . ";" . $trigger2 . ";" . $trigger3 . ";" . $trigger4 . ";";
+        $triggers = $trigger1 . ";" . $trigger2 . ";" . $trigger3 . ";" . $trigger4 . ";";
         
         // Inclui os triggers no banco
         $qr2 = $conn->exec($triggers);
